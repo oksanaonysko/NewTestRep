caad438ed05ce77b160a733ea41f8a663838ea5b
Oksana <oksana@test.com>
2023-03-19 16:45:08 +0200

    RunTests

--- TestRep-main/src/main/java/net/absoft/mortalWombat/AgeChecker.java
+++
@@ -1,9 +0,0 @@
-package net.absoft.mortalWombat;
-
-public class AgeChecker {
-    private final int MIN_AGE = 18;
-
-    public boolean canPlayGame (int age) {
-        return age >= MIN_AGE;
-    }
-}
